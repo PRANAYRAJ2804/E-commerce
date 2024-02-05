51e5ea3bdea5995ef1a64ef2b393210822327c40
PRANAYRAJ2804 <pranayraj2804@gmail.com>
2024-02-05 20:32:07 +0530

    navbar and pages created and routing added making hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory categoty="men"/>}/>
+      <Route path='/womens' element={<ShopCategory categoty="women"/>}/>
+      <Route path='/kids' element={<ShopCategory categoty="kid"/>}/>
+      <Route path='/product' element={<Product/>}>
+        <Route path='/productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+    </Routes>
+    </BrowserRouter>
+    
     </>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+// import logo from '../../../public/logo.png'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
+
+const Navbar = () => {
+    const [menu, setMenu] = useState("Shop")
+  return (
+    <>
+    <div className="navbar">
+        <div className="nav-logo">
+            <img src={logo} alt="logo" />
+            <p>WARDROBE SHOP</p>
+        </div>
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu("Shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link> {menu === "Shop" && <hr/>}</li>
+            <li onClick={()=>{setMenu("Men")}}><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link> {menu === "Men" && <hr/>}</li>
+            <li onClick={()=>{setMenu("Women")}}><Link style={{textDecoration: 'none'}} to='/Womens'>Women</Link> {menu === "Women" && <hr/>}</li>
+            <li onClick={()=>{setMenu("Kids")}}><Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link> {menu === "Kids" && <hr/>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <Link style={{textDecoration: 'none'}} to='/login'><button>Login</button></Link>
+            <Link style={{textDecoration: 'none'}} to='/cart'><img src={cart_icon} alt="cart-img" /></Link>
+            <div className="nav-cart-count">
+                0
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
